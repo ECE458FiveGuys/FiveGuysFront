1f62b3dfa9b06f3b48150d55bff2a9d06cbec6eb
Joe Heflin <jnh24@duke.edu>
2021-03-15 20:19:58 -0400

    label generation

--- src/app/Pages/MainPage/LabelGenerator.js
+++ src/app/Pages/MainPage/LabelGenerator.js
@@ -1,6 +1,6 @@
 import jsPDF from "jspdf";
 import JsBarcode from "jsbarcode";
-import {Canvas} from "canvas";
+import {Canvas, createCanvas} from "canvas";
 
 // Barcode label: An adhesive label affixed to assets bearing the text “HPT Asset” and the
 // asset number; the number is also shown as a barcode (Code 128 type C). Labels are 1.75”x0.5”
@@ -12,8 +12,10 @@ const LABEL_HEIGHT = 0.5;
 const HORIZONTAL_MARGIN = 0.28125;
 
 const LABELS_PER_COLUMN = 20;
+const LABELS_PER_PAGE = 80;
 
 const LEFT_SIDE_MARGIN = 0.33;
+const TOP_MARGIN = LABEL_HEIGHT;
 
 const LABEL_CONTAINER_WIDTH = LABEL_WIDTH + HORIZONTAL_MARGIN;
 const LABEL_CONTAINER_HEIGHT = LABEL_HEIGHT;
@@ -29,11 +31,10 @@ const PAGE_OPTIONS = {
 
 const LABEL_OPTIONS = {
     format: "CODE128C",
-    height: 0.5 - 0.1,
-    width: 1.75 - 0.2,
-    textAlign: "right",
+    height: 90,
+    width: 4,
+    textAlign: "left",
     textPosition: "bottom"
-
 }
 
 export function generateLabels(instruments) {
@@ -43,17 +44,27 @@ export function generateLabels(instruments) {
     let label_num = 0;
 
     instruments.forEach((instrument) => {
-        label_num++
+
+        if(label_num > LABELS_PER_PAGE) {
+            labelPage.addPage()
+            label_num = 0;
+        }
+
+        let data = instrument.asset_tag_number
         let text = "HTP Asset     " + instrument.asset_tag_number
-        const canvas = new Canvas()
-        JsBarcode(canvas, text, {...LABEL_OPTIONS})
+        let canvas = createCanvas(0,0,'svg');
+
+        JsBarcode(canvas, data,{text:text,...LABEL_OPTIONS});
 
         let row = label_num % LABELS_PER_COLUMN;
         let col = Math.floor(label_num / LABELS_PER_COLUMN);
 
         let x = col * LABEL_CONTAINER_WIDTH + LEFT_SIDE_MARGIN;
-        let y = row * LABEL_CONTAINER_HEIGHT;
-        labelPage.addImage(canvas, 'JPEG', x, y, LABEL_CONTAINER_WIDTH, LABEL_CONTAINER_HEIGHT);
+        let y = row * LABEL_CONTAINER_HEIGHT + TOP_MARGIN;
+
+        labelPage.addImage(canvas, 'jpeg', x, y, LABEL_WIDTH, LABEL_HEIGHT);
+
+        label_num++;
     })
 
     labelPage.save(`labels.pdf`) // Consider adding filters here in file descriptor
